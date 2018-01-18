0a23bd78455e772a52af8337205b408be3044f5d
Efrain Lopez <lopezefrain5894@gmail.com>
2018-01-18 22:16:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,55 @@
+
+function chocolateBars()
+{
+  return ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+}
+function addElementToBeginningOfArray(arrayReference, valueForNewElement)
+{
+  // declare a new array, passed value as the first pos, spread passed array
+  let temporaryArray = [valueForNewElement, ...arrayReference];
+  return temporaryArray;
+}
+function destructivelyAddElementToBeginningOfArray(arrayReference, element)
+{
+  // add element to beginn - mutated return
+  // mutates array - but returns length() of array
+  arrayReference.unshift(element);
+  // return object separately
+  return arrayReference;
+}
+function addElementToEndOfArray(arrayReference, element)
+{
+    let concatArray = arrayReference.concat(element);
+    return concatArray;
+}
+function destructivelyAddElementToEndOfArray(arrayReference, element)
+{
+  // a mutation will occur
+  // use the reference to permanently add a new final element
+  arrayReference.push(element);
+  return arrayReference;
+}
+function destructivelyAddElementToEndOfArray(arrayReference, element)
+{
+  // a mutation will occur
+  // use the reference to permanently add a new final element
+  arrayReference.push(element);
+  return arrayReference;
+}
+function accessElementInArray(array, index)
+{
+  // return the value/integer: 3 which is pointed to by array index position
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  // mutation will occur
+  // alter the reference
+  array.shift()
+  return array;
+}
+function removeElementFromBeginningOfArray(arrayReference)
+{
+  let copiedArray = arrayReference.slice(0, 1)
+  return copiedArray;
+}
